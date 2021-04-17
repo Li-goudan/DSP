60f3de6eb1810334935d7e570180a268e0584bbd
Li-goudan <57565588+Li-goudan@users.noreply.github.com>
2021-04-17 23:28:03 +0800

    DFT与FFT时间对比

---
+++ exper23.m
@@ -0,0 +1,52 @@
+%% DFT与FFT时间对比
+
+clc
+clear
+close all
+%初始值
+F = 50;
+N = 32;
+k = (0:(N-1));
+%步长0.000625
+l = 0.000625;
+t = (0:l:l*(N-1));
+%x为离散时间序列
+x = cos(2 * pi * F .* t);
+
+subplot(311);
+stem(k,x,'filled');
+xlabel('n');
+ylabel('x(n)');
+title('离散序列');
+%axis([0 (N-1) -2 2]);
+
+%FFT计时
+tic;
+y0 = fft(x,N);
+toc;
+%DFT计时
+tic;
+y1 = dft(x,N);
+toc;
+
+subplot(312);
+stem(k,abs(y0),'filled')
+xlabel('f');
+%ylabel('x(n)');
+title('DFT');
+%axis([-(N-1)/2 (N-1)/2 0 40]);
+
+subplot(313);
+stem(k,abs(y1),'filled')
+xlabel('f');
+%ylabel('');
+title('FFT');
+%axis([-(N-1)/2 (N-1)/2 0 40]);
+%%
+function [Xk] = dft(xn,N)
+n = 0:N-1;
+k = 0:N-1;
+%n'为n的转置
+nk = n'*k;
+Xk=xn * exp(-1i * 2 * pi / N) .^ nk;
+end
